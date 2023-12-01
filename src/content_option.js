719ddeff3a2d90d0028ef8daa8d88d308508d362
Naveen2070 <naveenrameshcud@gmail.com>
2023-12-01 20:12:18 +0530

    mail adderess updated

--- src/content_option.js
+++ src/content_option.js
@@ -1,6 +1,6 @@
-const logotext = "N";
+const logotext = 'N';
 const meta = {
-  title: "Naveen R",
+  title: 'Naveen R',
   description:
     "I'm Naveen, a technologist, technophile and MERN stack developer.",
 };
@@ -8,165 +8,165 @@ const meta = {
 const introdata = {
   title: "Hi, I'm Naveen",
   animated: {
-    first: "Technologist",
-    second: "Technophile",
-    third: "Web Developer",
-    fourth: "MERN stack developer",
-    fifth: "Cross Platform Developer",
-    sixth: "Programer",
+    first: 'Technologist',
+    second: 'Technophile',
+    third: 'Web Developer',
+    fourth: 'MERN stack developer',
+    fifth: 'Cross Platform Developer',
+    sixth: 'Programer',
     // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
   },
   description:
-    "An inquisitive mind surfing the waves of technology, blending creativity and curiosity to paint a unique portrait of the digital future.",
-  your_img_url: "https://i.ibb.co/wRjcnGL/me.jpg",
+    'An inquisitive mind surfing the waves of technology, blending creativity and curiosity to paint a unique portrait of the digital future.',
+  your_img_url: 'https://i.ibb.co/wRjcnGL/me.jpg',
 };
 
 const dataabout = {
-  title: "Who am I?",
+  title: 'Who am I?',
   aboutme:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 };
 const worktimeline = [
   {
-    jobtitle: "Example 1",
-    where: "Google",
-    date: "2023",
+    jobtitle: 'Example 1',
+    where: 'Google',
+    date: '2023',
   },
   {
-    jobtitle: "Example 2",
-    where: "Micosoft",
-    date: "2022",
+    jobtitle: 'Example 2',
+    where: 'Micosoft',
+    date: '2022',
   },
   {
-    jobtitle: "Example 3",
-    where: "Meta",
-    date: "2021",
+    jobtitle: 'Example 3',
+    where: 'Meta',
+    date: '2021',
   },
 ];
 
 const skills = [
   {
-    name: "Python",
+    name: 'Python',
   },
 
   {
-    name: "C",
+    name: 'C',
   },
 
   {
-    name: "Java",
+    name: 'Java',
   },
 
   {
-    name: "Bash",
+    name: 'Bash',
   },
 
   {
-    name: "Powershell",
+    name: 'Powershell',
   },
 
   {
-    name: "Reverse Engineering",
+    name: 'Reverse Engineering',
   },
 
   {
-    name: "Web-Exploitation",
+    name: 'Web-Exploitation',
   },
 
   {
-    name: "Network Exploitation",
+    name: 'Network Exploitation',
   },
 ];
 
 const dataportfolio = [
   {
-    img: "https://picsum.photos/400/?grayscale",
+    img: 'https://picsum.photos/400/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/800/?grayscale",
+    img: 'https://picsum.photos/400/800/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/?grayscale",
+    img: 'https://picsum.photos/400/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/600/?grayscale",
+    img: 'https://picsum.photos/400/600/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/300/?grayscale",
+    img: 'https://picsum.photos/400/300/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/700/?grayscale",
+    img: 'https://picsum.photos/400/700/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
 
   {
-    img: "https://picsum.photos/400/600/?grayscale",
+    img: 'https://picsum.photos/400/600/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/300/?grayscale",
+    img: 'https://picsum.photos/400/300/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/?grayscale",
+    img: 'https://picsum.photos/400/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/550/?grayscale",
+    img: 'https://picsum.photos/400/550/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/?grayscale",
+    img: 'https://picsum.photos/400/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
   {
-    img: "https://picsum.photos/400/700/?grayscale",
+    img: 'https://picsum.photos/400/700/?grayscale',
     description:
-      "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
   },
 ];
 
 const contactConfig = {
-  YOUR_EMAIL: "naveenrameshcud@gmail.com",
-  description: "Looking forward to hearing from you!",
+  YOUR_EMAIL: 'naveenrameshcud@gmail.com',
+  description: 'Looking forward to hearing from you!',
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-  YOUR_SERVICE_ID: "service_04lb2r9",
-  YOUR_TEMPLATE_ID: "template_nvonvpl",
-  YOUR_USER_ID: "ErUJqSHmC_GNgt8v1",
+  YOUR_SERVICE_ID: 'service_04lb2r9',
+  YOUR_TEMPLATE_ID: 'template_nvonvpl',
+  YOUR_USER_ID: 'ErUJqSHmC_GNgt8v1',
 };
 
 const socialprofils = {
-  github: "https://github.com/",
-  linkedin: "https://linkedin.com/",
+  github: 'https://github.com/Naveen2070',
+  linkedin: 'https://www.linkedin.com/in/naveen-r-550713204/',
 };
 export {
   meta,
